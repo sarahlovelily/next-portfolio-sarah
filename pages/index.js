4aa367a44d9de79e05733533fd2aad2eeec4dab2
sarah yang <sarahyang.frontend@gmail.com>
2019-07-12 12:00:23 +1000

    test now

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 
 const Index = () => (
   <Layout title="Home">
-    <p>Welcome to the home page</p>
+    <p>Welcome to the home page. This is a new next.js project.</p>
   </Layout>
 );
 
